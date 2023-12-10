084f934b367ffb76246232c3cdd5f8b238dd327a
Vu Nguyen <vu.nguyen@dmttek.com>
2023-12-11 01:52:59 +0700

    add favicon and site name using helmet

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,27 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import {
-  APP_INIT_ERROR, APP_READY, subscribe, initialize,
+  APP_INIT_ERROR, APP_READY, subscribe, initialize, getConfig,
 } from '@edx/frontend-platform';
 import {
   AppProvider, ErrorPage,
 } from '@edx/frontend-platform/react';
 import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
 import messages from './i18n';
 import './index.scss';
 import App from './App';
 
+
 subscribe(APP_READY, () => {
   ReactDOM.render(
     <AppProvider>
+      <Helmet>
+        <title>
+          Code | {getConfig().SITE_NAME}
+        </title>
+        <link rel="shortcut icon" href={getConfig().FAVICON_URL} type="image/x-icon" />
+      </Helmet>
       <App />
     </AppProvider>,
     document.getElementById('root'),
