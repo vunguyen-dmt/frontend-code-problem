d46fb7d8a3c7aadbb12e52d7b5ddffea2f7ca1c6
Vu Nguyen <vu.nguyen@dmttek.com>
2023-12-10 10:08:48 +0700

    only allow course creator fixing state, style

--- src/components/AccessDenied/AccessDenied.jsx
+++ src/components/AccessDenied/AccessDenied.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from '@edx/frontend-component-header';
+import Footer from '@edx/frontend-component-footer';
 
-const AccessDenied = () => {
-    return <p className='text-center mt-6'>Permission denied.</p>
-}
+const AccessDenied = () => (
+  <div>
+    <Header />
+    <p className="text-center mt-6 mb-6">Permission denied.</p>
+    <Footer />
+  </div>
+);
 
 export default AccessDenied;
